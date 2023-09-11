ea5be507ce2baa9466764c2d6e9192da33fcf41d
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-11 14:38:58 +0700

    style(cmake): fix the format of `package-lock.cmake`

--- package-lock.cmake
+++ package-lock.cmake
@@ -2,16 +2,16 @@
 # This file should be committed to version control
 
 # Format.cmake
-CPMDeclarePackage(Format.cmake
+cpmdeclarepackage(
+  Format.cmake
   VERSION 1.7.3
   GITHUB_REPOSITORY TheLartians/Format.cmake
   SYSTEM YES
-  EXCLUDE_FROM_ALL YES
-)
+  EXCLUDE_FROM_ALL YES)
 # Catch2
-CPMDeclarePackage(Catch2
+cpmdeclarepackage(
+  Catch2
   VERSION 3.4.0
   GITHUB_REPOSITORY catchorg/Catch2
   SYSTEM YES
-  EXCLUDE_FROM_ALL YES
-)
+  EXCLUDE_FROM_ALL YES)
