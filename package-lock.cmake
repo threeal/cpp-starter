ff5c1e9f3b32f253bc3da659da91d5752e216e24
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-09-11 14:37:04 +0700

    build(cmake): CPM use `package-lock.cmake`

---
+++ package-lock.cmake
@@ -0,0 +1,17 @@
+# CPM Package Lock
+# This file should be committed to version control
+
+# Format.cmake
+CPMDeclarePackage(Format.cmake
+  VERSION 1.7.3
+  GITHUB_REPOSITORY TheLartians/Format.cmake
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
+# Catch2
+CPMDeclarePackage(Catch2
+  VERSION 3.4.0
+  GITHUB_REPOSITORY catchorg/Catch2
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
