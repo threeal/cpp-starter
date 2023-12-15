ebf098d024d0638e75cf0e85d111d3543b4c7495
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-12-15 18:10:46 +0700

    feat: add executable target (#63)
    * feat: add `my_fibonacci_main` executable target
    
    * feat: use argparse to parse arguments in the main executable
    
    * ci: exclude `src/main.cpp` from test coverage check

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <argparse/argparse.hpp>
+#include <cstdlib>
+#include <iostream>
+#include <my_fibonacci/sequence.hpp>
+
+int main(int argc, char** argv) {
+  argparse::ArgumentParser program("my_fibonacci_main");
+  program.add_description(
+      "Generate a Fibonacci sequence up to the given number of terms.");
+  program.add_argument("n").help("The number of terms").scan<'i', int>();
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::exception& err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    return 1;
+  }
+
+  const auto n = program.get<int>("n");
+  const auto sequence = my_fibonacci::fibonacci_sequence(n);
+  for (auto val : sequence) {
+    std::cout << val << " ";
+  }
+  std::cout << std::endl;
+
+  return 0;
+}
