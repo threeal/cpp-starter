f929900577be0caea2fe43daaf591cf0406c7b3d
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-07 23:11:23 +0800

    build(cmake): declare CPM packages from `CPM.cmake` and remove `package-lock` file (#108)

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -4,4 +4,38 @@ file(
   ${CMAKE_BINARY_DIR}/_deps/CPM.cmake
   EXPECTED_MD5 14ea07dfb484cad5db4ee1c75fd6a911
 )
+
 include(${CMAKE_BINARY_DIR}/_deps/CPM.cmake)
+
+cpmdeclarepackage(
+  argparse
+  VERSION 3.0
+  GITHUB_REPOSITORY p-ranav/argparse
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
+
+cpmdeclarepackage(
+  CheckWarning.cmake
+  VERSION 2.0.1
+  GITHUB_REPOSITORY threeal/CheckWarning.cmake
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
+
+cpmdeclarepackage(
+  Format.cmake
+  VERSION 1.7.3
+  GITHUB_REPOSITORY TheLartians/Format.cmake
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+  OPTIONS "FORMAT_SKIP_CMAKE ON"
+)
+
+cpmdeclarepackage(
+  Catch2
+  VERSION 3.5.1
+  GITHUB_REPOSITORY catchorg/Catch2
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
