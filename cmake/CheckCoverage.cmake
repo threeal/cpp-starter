43aa1b2891927227e48e0b497faf8bf6982842ac
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-07 22:44:54 +0800

    build(cmake): add `CheckCoverage.cmake` module file (#106)

---
+++ cmake/CheckCoverage.cmake
@@ -0,0 +1,20 @@
+function(target_check_coverage TARGET)
+  if(MSVC)
+    message(WARNING "Test coverage check is not available on MSVC")
+    return()
+  endif()
+
+  target_compile_options(${TARGET} PRIVATE --coverage -O0 -fno-exceptions)
+  target_link_options(${TARGET} PRIVATE --coverage)
+
+  get_target_property(TARGET_BINARY_DIR ${TARGET} BINARY_DIR)
+  get_target_property(TARGET_SOURCES ${TARGET} SOURCES)
+
+  foreach(SOURCE ${TARGET_SOURCES})
+    set(GCDA ${TARGET_BINARY_DIR}/CMakeFiles/${TARGET}.dir/${SOURCE}.gcda)
+    add_custom_command(
+      TARGET ${TARGET} PRE_LINK
+      COMMAND ${CMAKE_COMMAND} -E rm -f ${GCDA}
+    )
+  endforeach()
+endfunction()
