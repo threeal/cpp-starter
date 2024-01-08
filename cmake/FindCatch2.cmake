590e2bfe60c6388c278c9f8e8612d88cf07aea72
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-08 22:36:53 +0700

    build: add `FindCatch2.cmake`

---
+++ cmake/FindCatch2.cmake
@@ -0,0 +1,13 @@
+if(Catch2_FOUND)
+  return()
+endif()
+
+find_package(Catch2 QUIET CONFIG)
+if(Catch2_FOUND)
+  return()
+endif()
+
+include(CPM)
+cpmgetpackage(Catch2)
+
+list(APPEND CMAKE_MODULE_PATH ${Catch2_SOURCE_DIR}/extras)
