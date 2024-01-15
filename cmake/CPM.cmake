5b659b09c0eddedb6f47124612f6554970f4cf3f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-15 21:40:43 +0700

    build: utilize FixFormat.cmake

--- cmake/CPM.cmake
+++ cmake/CPM.cmake
@@ -15,6 +15,10 @@ function(cpm_add_check_warning)
   cpmaddpackage(gh:threeal/CheckWarning.cmake@2.1.0)
 endfunction()
 
+function(cpm_add_fix_format)
+  cpmaddpackage(gh:threeal/FixFormat.cmake@1.0.0)
+endfunction()
+
 function(cpm_add_format)
   cpmaddpackage(
     Format.cmake
