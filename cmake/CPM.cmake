c160bcda02049fbded15485337d770b1df9ff2f9
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-04-09 16:20:36 +0700

    build: simplify dependency find package (#135)
    * build: replace `Findargparse.cmake` with direct `find_package` call
    
    * build: replace `FindCatch2.cmake` with direct `find_package` call
    
    * build: replace `FindCheckWarning.cmake` with direct `find_package` call
    
    * build: replace `FindFixFormat.cmake` with direct `find_package` call
    
    * build: replace `CPM.cmake` with `get_cpm` function

--- cmake/CPM.cmake
+++
@@ -1,8 +0,0 @@
-file(
-  DOWNLOAD
-  https://github.com/cpm-cmake/CPM.cmake/releases/download/v0.38.7/CPM.cmake
-  ${CMAKE_BINARY_DIR}/_deps/CPM.cmake
-  EXPECTED_MD5 14ea07dfb484cad5db4ee1c75fd6a911
-)
-
-include(${CMAKE_BINARY_DIR}/_deps/CPM.cmake)
