89d07de38b107783352c1a10dc36eaff3d945073
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-05-27 22:23:49 +0700

    test: utilize UT for testing (#152)
    * test: utilize UT for testing
    
    * test: disable UT module

--- test/sequence_test.cpp
+++ test/sequence_test.cpp
@@ -1,11 +1,16 @@
 #include <my_fibonacci/sequence.hpp>
 
-#include <catch2/catch_test_macros.hpp>
+#include <boost/ut.hpp>
 
-TEST_CASE("test fibonacci sequence") {
-  CHECK(my_fibonacci::fibonacci_sequence(-1) == std::vector<int>{});
-  CHECK(my_fibonacci::fibonacci_sequence(0) == std::vector<int>{});
-  CHECK(my_fibonacci::fibonacci_sequence(1) == std::vector<int>{1});
-  CHECK(my_fibonacci::fibonacci_sequence(2) == std::vector<int>{1, 1});
-  CHECK(my_fibonacci::fibonacci_sequence(5) == std::vector<int>{1, 1, 2, 3, 5});
+using my_fibonacci::fibonacci_sequence;
+using namespace boost::ut;
+
+int main() {
+  should("generate a Fibonacci sequence") = [] {
+    expect(fibonacci_sequence(-1) == std::vector<int>{});
+    expect(fibonacci_sequence(0) == std::vector<int>{});
+    expect(fibonacci_sequence(1) == std::vector<int>{1});
+    expect(fibonacci_sequence(2) == std::vector<int>{1, 1});
+    expect(fibonacci_sequence(5) == std::vector<int>{1, 1, 2, 3, 5});
+  };
 }
